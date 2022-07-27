dd0f3d36a739108dab43b89a6eb2d00b0142855b
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-27 10:15:58 -0300

    fix: package.json test script and other minor fixes

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import express from "express";
 import "express-async-errors";
 import { errorHandlerMiddleware } from "./middlewares/errorHandlerMiddleware.js";
-import recommendationRouter from "./routers/recommendationRouter.js";
+import recommendationRouter from "./routes/recommendationRouter.js";
 
 const app = express();
 app.use(cors());
--- back-end/tests/app.test.ts
+++ back-end/tests/app.test.ts
@@ -1,8 +1,8 @@
-﻿import supertest from "supertest";
-import { faker } from "@faker-js/faker";
+﻿import { faker } from "@faker-js/faker";
+import { prisma } from "../src/database.js";
+import supertest from "supertest";
 
 import app from "../src/app.js";
-import { prisma } from "../src/database.js";
 import scenarioFactory from "./factories/scenarioFactory.js";
 
 import { CreateRecommendationData } from "../src/services/recommendationsService.js";
@@ -11,26 +11,32 @@ beforeAll(async () => {
   await prisma.recommendation.deleteMany();
 });
 
+const agent = supertest(app);
+
 describe("recommendation tests", () => {
   it("should return status code 201 if created", async () => {
     const recommendation: CreateRecommendationData = {
       name: faker.name.firstName(),
       youtubeLink: faker.internet.url(),
     };
-    const response = await supertest(app).post("/").send(recommendation);
+    const response = await agent.post("/").send(recommendation);
     expect(response.status).toBe(201);
 
     const createdRecommendation = await prisma.recommendation.findFirst({
       where: { name: recommendation.name },
     });
     expect(createdRecommendation).not.toBeNull();
-  });
+  }, 10000);
 
   it("should return all found recommendations and status code 200", async () => {
-    const recommendations = scenarioFactory.createScenarioWithFiveRecommendations();
-    const response = await supertest(app).get("/");
+    const recommendations = await scenarioFactory.createScenarioWithFiveRecommendations();
+    const response = await agent.get("/");
 
     expect(response.status).toBe(200);
     expect(response.body).toEqual(recommendations);
-  });
+  }, 10000);
+});
+
+afterAll(async () => {
+  await prisma.$disconnect();
 });
